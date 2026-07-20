310acd8b25641bd647914efedd889cbf0fe0da89
siddhibhandari29 <siddhibhandari129@gmail.com>
2026-07-21 03:09:00 +0530

    Delete firebase-config (1).js

--- firebase-config (1).js
+++
@@ -1,9 +0,0 @@
-// Paste the config object from your Firebase project here.
-export const firebaseConfig = {
-  apiKey: "AIzaSyD-_VcWYfSeeHBSn_SMy6ONVRUrvyHZnK0",
-  authDomain: "study-notes-generator-f3d2a.firebaseapp.com",
-  projectId: "study-notes-generator-f3d2a",
-  storageBucket: "study-notes-generator-f3d2a.firebasestorage.app",
-  messagingSenderId: "464800565709",
-  appId: "1:464800565709:web:d2fade0433db8620505c84"
-};
